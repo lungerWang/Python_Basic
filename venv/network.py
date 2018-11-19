584ddf26285a4066cc81a7610e6f20aaed32f537
Allen <836414757@qq.com>
2018-11-19 10:06:38 +0800

    test tcp server

--- venv/network.py
+++ venv/network.py
@@ -1,22 +1,52 @@
 # Socket是网络编程的一个抽象概念。通常我们用一个Socket表示打开了一个网络连接，而打开一个Socket需要知道目标ip地址、端口号及连接协议
 # 建立tcp连接时，主动发起的一方叫做客户端，被动连接的一方叫服务器
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('www.sina.com.cn', 80))
-# 发送数据:
-s.send(b'GET / HTTP/1.1\r\nHost: www.sina.com.cn\r\nConnection: close\r\n\r\n')
-buffer = []
-while True:
-    d = s.recv(1024)
-    if d:
-        buffer.append(d)
-    else:
-        break
+# tcp客户端
+# import socket
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.connect(('www.sina.com.cn', 80))
+# # 发送数据:
+# s.send(b'GET / HTTP/1.1\r\nHost: www.sina.com.cn\r\nConnection: close\r\n\r\n')
+# buffer = []
+# while True:
+#     d = s.recv(1024)
+#     if d:
+#         buffer.append(d)
+#     else:
+#         break
+#
+# data = b''.join(buffer)
+# s.close()
+# print(data)
+# header, html = data.split(b'\r\n\r\n', 1)
+# print(header.decode('utf-8'))
+# with open('sina.html', 'wb') as f:
+#     f.write(html)
 
-data = b''.join(buffer)
-s.close()
-print(data)
-header, html = data.split(b'\r\n\r\n', 1)
-print(header.decode('utf-8'))
-with open('sina.html', 'wb') as f:
-    f.write(html)
+# tcp服务器
+import threading, socket, time
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('127.0.0.1', 9999))
+s.listen(5)
+print('Waitting for connection....')
+
+def tcplink(sock, addr):
+    print('Accept new connection from %s:%s...' % addr)
+    sock.send(b'Welcome!')
+    while True:
+        data = sock.recv(1024)
+        time.sleep(1)
+        if not data or data.decode('utf-8') == 'exit':
+            break
+        sock.send(('Hello, %s!' % data.decode('utf-8')).encode('utf-8'))
+    sock.close()
+    print('Connection from %s:%s closed.' % addr)
+
+    while True:
+        # 接受一个新连接:
+        sock, addr = s.accept()
+        # 创建新线程来处理TCP连接:
+        t = threading.Thread(target=tcplink, args=(sock, addr))
+        t.start()
+
+
+
