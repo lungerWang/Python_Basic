c8a7c2b80039e7036d1bfa622186c9265468f874
Allen <836414757@qq.com>
2018-11-20 10:36:17 +0800

    sql

---
+++ venv/sql.py
@@ -0,0 +1,19 @@
+import sqlite3
+# create table and insert
+# conn = sqlite3.connect('test.db')
+# cursor = conn.cursor()
+# cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
+# cursor.execute('insert into user(id, name) values (\'1\', \'Michael\')')
+# print(cursor.rowcount)
+# cursor.close()
+# conn.commit()
+# conn.close()
+
+# query
+# conn = sqlite3.connect('test.db')
+# cursor = conn.cursor()
+# cursor.execute('select * from user where id=?', ('1',))
+# result = cursor.fetchall()
+# print(result)
+# cursor.close()
+# conn.close()
